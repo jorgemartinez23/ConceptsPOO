e59e108b5d8f2c994c58752ea9c4afa09b19c163
Jorge Edgardo Adrian Martinez <jorgemartinez_2304@hotmail.com>
2022-08-13 21:05:33 -0300

    Finish date class

---
+++ ConceptsPoo/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using ConceptsPoo;
+
+Console.WriteLine("Hello, World!");
+
+try
+{
+	Console.WriteLine(new Date(2022, 2, 11));
+	Console.WriteLine(new Date(1985, 4, 23));
+	Console.WriteLine(new Date(1987, 5, 12));
+}
+catch (Exception error )
+{
+
+	Console.WriteLine(error.Message);
+}
+Console.Read();
