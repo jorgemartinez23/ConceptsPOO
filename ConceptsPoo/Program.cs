ceb39bc0b6aab013cf7694b1a780b450103bbded
Jorge Edgardo Adrian Martinez <jorgemartinez_2304@hotmail.com>
2022-08-14 21:22:07 -0300

    Employee, SalaryEmployee & CommissionEmployee Finish

--- ConceptsPoo/Program.cs
+++ ConceptsPoo/Program.cs
@@ -3,15 +3,5 @@ using ConceptsPoo;
 
 Console.WriteLine("Hello, World!");
 
-try
-{
-	Console.WriteLine(new Date(2022, 2, 11));
-	Console.WriteLine(new Date(1985, 4, 23));
-	Console.WriteLine(new Date(1987, 5, 12));
-}
-catch (Exception error )
-{
-
-	Console.WriteLine(error.Message);
-}
+Employee employee1 = new SalaryEmployee();
 Console.Read();
